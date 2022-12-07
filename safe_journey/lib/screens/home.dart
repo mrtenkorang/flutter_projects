5791289002734ba56b3d0df347314a6af5864f8a
Tenkorang17 <josephoforitenkorang390@gmail.com>
2022-12-07 15:19:37 +0000

    Updates

--- safe_journey/lib/screens/home.dart
+++ safe_journey/lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:safe_journey/accounts/login_page.dart';
 import 'package:safe_journey/accounts/registration_page.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -15,6 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Row(
           children: [
@@ -35,10 +37,9 @@ class _HomeScreenState extends State<HomeScreen> {
               child: const Text(
                 'Login',
                 style: TextStyle(
-                  fontFamily: 'Quicksand',
-                  color: Colors.white,
-                  fontSize: 20.0
-                ),
+                    fontFamily: 'Quicksand',
+                    color: Colors.white,
+                    fontSize: 20.0),
               ),
             ),
             TextButton(
@@ -50,8 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: TextStyle(
                     fontFamily: 'Quicksand',
                     color: Colors.white,
-                    fontSize: 20.0
-                ),
+                    fontSize: 20.0),
               ),
             ),
           ],
@@ -59,7 +59,60 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: const Color.fromRGBO(7, 8, 89, 1),
       ),
       body: Column(
-        children: [],
+        children: [
+          AnimatedTextKit(
+            repeatForever: true,
+            animatedTexts: [
+              TypewriterAnimatedText(
+                'Welcome',
+                textStyle: const TextStyle(
+                    fontFamily: 'Quicksand',
+                    fontWeight: FontWeight.w900,
+                    fontSize: 40.0),
+              ),
+              TypewriterAnimatedText(
+                'To',
+                textStyle: const TextStyle(
+                    fontFamily: 'Quicksand',
+                    fontWeight: FontWeight.w900,
+                    fontSize: 40.0),
+              ),
+              TypewriterAnimatedText(
+                'Safe Travel',
+                textStyle: const TextStyle(
+                    fontFamily: 'Quicksand',
+                    fontWeight: FontWeight.w900,
+                    fontSize: 40.0),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 170.0,
+            child: Image.asset('images/bus.png'),
+          ),
+          const Text(
+            'What is the location?',
+            style: TextStyle(
+              color: Color.fromRGBO(7, 8, 89, 1),
+              fontSize: 30.0,
+              fontFamily: 'Quicksand',
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 30.0,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(7, 8, 89, 1),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              
+            ),
+          ),
+        ],
       ),
     );
   }
