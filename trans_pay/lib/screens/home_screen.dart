b295217eff3b945e6f4038c7c3a6380aa7ec4b9a
Tenkorang17 <josephoforitenkorang390@gmail.com>
2022-12-11 06:23:57 +0000

    Update

--- trans_pay/lib/screens/home_screen.dart
+++ trans_pay/lib/screens/home_screen.dart
@@ -6,24 +6,56 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(248, 248, 248, 1),
-      body: Column(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-            ),
-            label: 'Home'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
+      appBar: AppBar(
+        elevation: 0.1,
+        backgroundColor: const Color.fromRGBO(248, 248, 248, 1),
+        title: Row(
+          children: const [
+            Text(
+              'TransPay',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Montserrat'
               ),
-              label: 'Home'
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
+      backgroundColor: const Color.fromRGBO(248, 248, 248, 1),
+      body: Column(
+        children: [],
+      ),
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 10.0,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history_edu),
+              label: 'history',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.dashboard,
+              ),
+              label: 'dashboard',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: 'notification',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle_outlined),
+              label: 'profile',
+            ),
+          ],
+        ),
     );
   }
 }
