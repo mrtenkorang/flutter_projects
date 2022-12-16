c4a013a1cfbecbe120e65f0767c52b7406ff8e19
Tenkorang17 <josephoforitenkorang390@gmail.com>
2022-12-16 05:53:36 +0000

    Update

--- trans_pay/lib/main.dart
+++ trans_pay/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:trans_pay/screens/home_screen.dart';
 import 'package:trans_pay/screens/init_screen.dart';
@@ -6,8 +7,15 @@ import 'package:trans_pay/screens/send_screen.dart';
 import 'package:trans_pay/screens/signup_screen.dart';
 import 'package:trans_pay/screens/topup_screen.dart';
 import 'package:trans_pay/screens/withdraw_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const Trans());
 }
 
